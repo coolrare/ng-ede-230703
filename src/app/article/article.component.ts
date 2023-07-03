8fa5bec88194e2ba26988e98958d16c046c8fc54
Will <doggy.huang@gmail.com>
2023-07-03 13:59:24 +0800

    建立 ArticleComponent 並設定 @Input() 傳入資料顯示

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent {
+  @Input()
+  i = 0;
+
+  @Input()
+  item: any = {};
+}
