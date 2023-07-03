b99941926e3e937430639e6a2b38cf537234916c
Will <doggy.huang@gmail.com>
2023-07-03 14:43:58 +0800

    示範 Paste JSON as Type 自動產生 interface

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -3,17 +3,27 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
 })
 export class ArticleComponent {
   @Input() i = 0;
 
-  @Input() item: any = {};
+  @Input() item: Article = {} as Article;
 
   @Output() delete = new EventEmitter<number>();
 
   deleteArticle() {
     this.delete.emit(this.item.id);
   }
+}
 
+export interface Article {
+  id: number;
+  href: string;
+  title: string;
+  date: string;
+  author: string;
+  category: string;
+  'category-link': string;
+  summary: string;
 }
